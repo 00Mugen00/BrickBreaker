94faea02c72981b3cc5318335b11f06716190216
00Mugen00 <genericblue36@gmail.com>
2019-01-08 21:52:07 +0100

    Paddle Movement added

---
+++ BrickBreaker/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+
+    [SerializeField]
+    float screenWidthInUnits = 19f;
+    [SerializeField]
+    float minX = 1f;
+    [SerializeField]
+    float maxX = 18f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mousePositionInUnits, minX, maxX);
+        transform.position = paddlePosition;
+    }
+}
