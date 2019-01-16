37dfb60fcd2228ae864df93591540a39fbf8b9de
00Mugen00 <genericblue36@gmail.com>
2019-01-16 22:07:20 +0100

    Go to next level when all bricks have been destroyed

--- BrickBreaker/Assets/Scripts/BrickController.cs
+++ BrickBreaker/Assets/Scripts/BrickController.cs
@@ -7,9 +7,18 @@ public class BrickController : MonoBehaviour
     [SerializeField]
     AudioClip breakSound;
 
+    LevelManager levelManager;
+
+    private void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+        levelManager.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        levelManager.BlockDestroyed();
     }
 }
---
+++ BrickBreaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    int breakableBlocks;
+
+    BrickBreakerSceneManager sceneManager;
+
+    private void Start()
+    {
+        sceneManager = FindObjectOfType<BrickBreakerSceneManager>();
+    }
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneManager.LoadNextScene();
+        }
+    }
+}
